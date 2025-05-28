8ae256fa42024b62e2ca5a54a1a8ed652cda5dac
Hassan-Jarkko <dev@mccoin.com>
2025-05-28 11:09:43 +0400

    removed expermintal webpack

--- next.config.ts
+++ next.config.ts
@@ -17,29 +17,7 @@ const nextConfig: NextConfig = {
         hostname: 'cryptoramble.com', 
       }
     ],
-  },
-  webpack: (config, options) => {
-    // Add canvas to externals
-    config.externals = [...(config.externals || []), { canvas: "canvas" }];
-
-    if (!options.isServer) {
-      // Use null-loader for canvas in client-side builds
-      config.module.rules.push({
-        test: /canvas/,
-        use: "null-loader",
-      });
-
-      // Set fallback for canvas to false
-      config.resolve.fallback = {
-        ...(config.resolve.fallback || {}),
-        canvas: false,
-      };
-    }
-    return config;
-  },
-  experimental: {
-    esmExternals: "loose", // required for canvas to work
-  },
+  }
 };
 
 const withNextIntl = createNextIntlPlugin();
